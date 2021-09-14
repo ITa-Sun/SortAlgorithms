7a89d2e0f6060ffa6c7a3b9e6959b48a03d84dde
ITa-Sun <68020072+ITa-Sun@users.noreply.github.com>
2021-09-14 22:10:45 +0800

    终于回来了

---
+++ test/SortTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class SortTest {
+    int[] arr = new int[]{1,9,3,4,8,5,7};
+    @Test
+    public void TestSort(){
+
+       // BubbleSort.doSort(arr);
+//        SelectionSort.doSort(arr);
+        InsertSort.doSort(arr);
+    }
+}
